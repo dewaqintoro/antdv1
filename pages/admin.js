6ef995817eb31000da0fe7bd4eb8e93ec84f5b25
dewaqintoro97 <dewa.qintoro@students.amikom.ac.id>
2020-08-15 18:30:06 +0700

    card

--- pages/admin.js
+++ pages/admin.js
@@ -5,6 +5,7 @@ import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/i
 import Link from 'next/link'
 const { Header, Content, Footer, Sider } = Layout;
 import SideCompDew from './components/SideCompDew';
+import Kasus from './kasus';
  
 class AdminPage extends Component {
 
@@ -16,11 +17,7 @@ class AdminPage extends Component {
         <Layout>
           <Content style={{ margin: '24px 16px 0' }}>
             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              {/* <Kasus/> */}
-              <h1>Admin Page</h1>
-              <div className="adminPage">
-                <p></p>
-              </div>
+              <Kasus/>
             </div>
           </Content>
         </Layout>
